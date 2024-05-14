631a6d734e8f5705aba2411cf5ffd9b13275c567
APIVerve Team <hello@apiverve.com>
2024-05-14 15:29:35 -0500

    Bug fixes and Improvements

---
+++ loremipsumgeneratorQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class loremipsumgeneratorQueryOptions {
+public int sentences { get; set; }
+public int words { get; set; }
+}
+
+}
