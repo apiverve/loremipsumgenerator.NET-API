d80c99589b5fa307bdb833ce9a9717517670910f
APIVerve Team <hello@apiverve.com>
2024-08-20 10:57:58 -0500

    Bug Fixes

--- LoremIpsumGeneratorAPIClient.cs
+++ LoremIpsumGeneratorAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, loremipsumgeneratorQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, LoremIpsumGeneratorQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(loremipsumgeneratorQueryOptions options = null)
+        public ResponseObj Execute(LoremIpsumGeneratorQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(loremipsumgeneratorQueryOptions options)
+        private string constructURL(LoremIpsumGeneratorQueryOptions options)
         {
             string url = _apiEndpoint;
 
