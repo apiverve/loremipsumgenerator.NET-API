080960ee2f644601f4be5aebaebebdc8bfd38107
APIVerve Team <hello@apiverve.com>
2024-08-20 10:58:00 -0500

    Bug Fixes

---
+++ LoremIpsumGeneratorQueryOptions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class LoremIpsumGeneratorQueryOptions {
+public int paragraphs { get; set; }
+public bool startwithlorem { get; set; }
+public int maxlength { get; set; }
+}
+
+}
